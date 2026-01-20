cd3beed28b6d9897380f268a362ab7509b78a6a7
Harishbabu2005 <harishbabups2005@gmail.com>
2026-01-20 11:27:09 +0530

    just for looking the frontend

---
+++ scholarship-frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import "../styles/theme.css";
+
+function Login() {
+  return (
+    <div className="card">
+      <h1>Login</h1>
+      <p>Login form will be added here</p>
+    </div>
+  );
+}
+
+export default Login;
---
+++ scholarship-frontend/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import "../styles/theme.css";
+
+function Signup() {
+  return (
+    <div className="card">
+      <h1>Sign Up</h1>
+      <p>Registration form will be added here</p>
+    </div>
+  );
+}
+
+export default Signup;
